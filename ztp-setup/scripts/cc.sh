7aa3681e044aea523cc2fef293c9e435655c442e
jnpr-rtulsian <rtulsian@juniper.net>
2019-02-06 14:42:46 -0800

    Cleaning up

--- ztp-setup/scripts/cc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-systemctl disable firewalld
-systemctl stop firewalld
-docker run -t --net host -e orchestrator=openstack -e action=import_cluster -v $COMMAND_SERVERS_FILE:/command_servers.yml -v $INSTANCES_FILE:/instances.yml -d --privileged --name contrail_command_deployer $CCD_IMAGE
-docker logs -f contrail_command_deployer
